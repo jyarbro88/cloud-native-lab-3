1907be27a796809d1c83a97ec2f9041c2dfa2ab5
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-03 10:26:51 -0500

    initial commit

---
+++ src/main/java/com/lab3/simpleweb/SimpleWebApplication.java
@@ -0,0 +1,12 @@
+package com.lab3.simpleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebApplication.class, args);
+    }
+}
